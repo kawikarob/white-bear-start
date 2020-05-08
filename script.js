fe424684d343cb8188104e4aab767faf42416f9e
kawikarob <64801223+kawikarob@users.noreply.github.com>
2020-05-07 23:09:03 -0700

    add pages

---
+++ script.js
@@ -0,0 +1,7 @@
+$("#save-imagery").click(function () {
+   $("#overlay-success").toggleClass("d-flex d-none");
+});
+
+$("#error-imagery").click(function () {
+   $("#overlay-error").toggleClass("d-flex d-none");
+});
