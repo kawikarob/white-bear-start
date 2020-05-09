07ad2d781a9b365e2de786d5a847af7bae3c8cdf
kawikarob <64801223+kawikarob@users.noreply.github.com>
2020-05-08 19:19:26 -0700

    add new pages

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ $("#save-imagery").click(function () {
 $("#error-imagery").click(function () {
    $("#overlay-error").toggleClass("d-flex d-none");
 });
+
+//https://stackoverflow.com/a/6003829
+
+function moveCaretToEnd(el) {
+   if (typeof el.selectionStart == "number") {
+      el.selectionStart = el.selectionEnd = el.value.length;
+   } else if (typeof el.createTextRange != "undefined") {
+      el.focus();
+      var range = el.createTextRange();
+      range.collapse(false);
+      range.select();
+   }
+}
+
+var textarea = document.getElementById("test");
+
+textarea.onfocus = function () {
+   moveCaretToEnd(textarea);
+
+   // Work around Chrome's little problem
+   window.setTimeout(function () {
+      moveCaretToEnd(textarea);
+   }, 1);
+};
